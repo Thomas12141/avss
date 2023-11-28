4c5b64114c6906c6867823c70d2504683a10117e
thomas <tf670992@fh-muenster.de>
2023-11-28 18:25:06 +0100

    ToString and equals implementation and the implementation for the data.

--- src/Boot/Boot.java
+++ src/Boot/Boot.java
@@ -2,6 +2,23 @@ package Boot;
 public class Boot {
     private String id, verliehen, ausleihdatum, rueckhgabedatum, kundennname;
 
+
+    public Boot(String id, String verliehen, String ausleihdatum, String rueckhgabedatum, String kundennname) {
+        this.id = id;
+        this.verliehen = verliehen;
+        this.ausleihdatum = ausleihdatum;
+        this.rueckhgabedatum = rueckhgabedatum;
+        this.kundennname = kundennname;
+    }
+
+    public Boot(String id) {
+        this.id = id;
+        this.verliehen = "nein";
+        this.ausleihdatum = null;
+        this.rueckhgabedatum = null;
+        this.kundennname = null;
+    }
+
     public void setVerliehen(String verliehen) {
         this.verliehen = verliehen;
     }
@@ -37,12 +54,16 @@ public class Boot {
     public String getKundennname() {
         return kundennname;
     }
+    @Override
+    public String toString() {
+        if(verliehen.equals("ja"))
+            return "Id: " + id + "\t" + "Kunde: " + kundennname + "\t" + "Verliehen am: " + ausleihdatum + "\t" + "bis: " + rueckhgabedatum + ".";
+        else
+            return "Id: " + id + "\t" + "Bereit um zu leihen.";
+    }
 
-    public Boot(String id, String verliehen, String ausleihdatum, String rueckhgabedatum, String kundennname) {
-        this.id = id;
-        this.verliehen = verliehen;
-        this.ausleihdatum = ausleihdatum;
-        this.rueckhgabedatum = rueckhgabedatum;
-        this.kundennname = kundennname;
+    @Override
+    public boolean equals(Object obj) {
+        return this.id.equals(((Boot)obj).id);
     }
 }
--- src/Data/Data.java
+++ src/Data/Data.java
@@ -26,6 +26,28 @@ public class Data {
     static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
     static File xmlFile = new File("myXML.xml");
 
+    public static void addNewElement(String id) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = builder.parse(xmlFile);
+
+        Element root = document.getDocumentElement();
+
+        Element boot = document.createElement("Boot");
+        boot.setAttribute("ID", id);
+        boot.setIdAttribute("ID", true);
+        root.appendChild(boot);
+
+        Element verliehenElement = document.createElement("Verliehen");
+        verliehenElement.appendChild(document.createTextNode("nein"));
+        boot.appendChild(verliehenElement);
+
+        DOMSource domSource = new DOMSource(document);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        StreamResult result = new StreamResult(xmlFile);
+        transformer.transform(domSource, result);
+    }
+
     public static void addNewElement(String id, String verliehen, String ausleihdatum, String rueckhgabedatum,
                                      String kundennname) throws ParserConfigurationException, IOException, SAXException, TransformerException {
 
@@ -95,19 +117,28 @@ public class Data {
         NodeList nodeList = element.getChildNodes();
         ArrayList<Boot> list = new ArrayList<>();
         int lenght = nodeList.getLength();
+        ArrayList<Boot> verliehenList = new ArrayList<>();
+        ArrayList<Boot> nichtVerliehenList = new ArrayList<>();
         for (int i = 0; i < lenght; i++) {
             if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE) {
                 Element el = (Element) nodeList.item(i);
-                System.out.println(el);
                 String id = el.getAttributes().item(0).toString().substring(4, el.getAttributes().item(0).toString().length()-1);
-                String verliehen;
-                Boot boot;
+                String verliehen = el.getElementsByTagName("Verliehen").item(0).getTextContent();
+                if(verliehen.equals("ja")){
+                    String ausleihdatum = el.getElementsByTagName("Ausleihdatum").item(0).getTextContent();
+                    String rueckgabedatum = el.getElementsByTagName("Rueckgabedatum").item(0).getTextContent();
+                    String kundenname = el.getElementsByTagName("Kundenname").item(0).getTextContent();
+                    Boot boot = new Boot(id, verliehen, ausleihdatum, rueckgabedatum, kundenname);
+                    verliehenList.add(boot);
+                }else {
+
+                    Boot boot = new Boot(id);
+                    verliehenList.add(boot);
+                }
             }
         }
+        list.addAll(verliehenList);
+        list.addAll(nichtVerliehenList);
         return list;
-    }
-    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, SAXException {
-        //addNewElement("510", "ja", "14125", "35235", "thah");
-        ArrayList list = getAllElements();
     }
 }
